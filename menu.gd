8e4f8e514815ddaf150bd3941c2e885389148436
Ruarhid <vladislav-shachnev@yandex.ru>
2025-03-22 20:33:40 +0700

    screenmode

--- menu.gd
+++ menu.gd
@@ -1,13 +1,66 @@
 extends Control
 
 
+
+
+# Список разрешений
+var resolutions = [
+	[1920, 1080],
+	[1280, 720],
+	[800, 600]
+]
+
+var screen_mode = [
+	["Оконный режим", 0],
+	["Полноэкранный режим", 3],
+	["Эксклюзивный полноэкранный режим", 4]
+]
+
+
+
 func _on_ready() -> void:
+	
 	$AudioStreamPlayer.play()
+	$AudioStreamPlayer.volume_linear = $MarginContainer/P_Settings/VBoxContainer/HSlider.value
 	$MarginContainer/P_Settings.visible = false
 	$MarginContainer/P_Main.visible = true
-	print("START")
+	
+	
+	
+	var option_button2 = $MarginContainer/P_Settings/VBoxContainer/OptionButton2
+	for mode in screen_mode:
+		option_button2.add_item("%s" % mode[0])
+		
 
-func _on_play_pressed() -> void:
+
+# Заполняем OptionButton
+	var option_button = $MarginContainer/P_Settings/VBoxContainer/OptionButton
+	for res in resolutions:
+		option_button.add_item("%dx%d" % [res[0], res[1]])
+	
+	# Подключаем сигнал
+	option_button.item_selected.connect(_on_resolution_selected)
+	option_button2.item_selected.connect(_on_screen_mode_selected)
+
+
+func _on_screen_mode_selected(index: int):
+	var screen_mode_selected = screen_mode[index][1]
+	DisplayServer.window_set_mode(screen_mode_selected)
+ 
+
+
+
+func _on_resolution_selected(index: int):
+	# Получаем выбранное разрешение
+	var selected_resolution = resolutions[index]
+	
+	# Устанавливаем размер окна
+	DisplayServer.window_set_size(Vector2i(selected_resolution[0], selected_resolution[1]))
+	
+
+
+
+func _on_play_pressed() -> void: 
 	pass # Replace with function body.
 
 func _on_settings_pressed() -> void:
